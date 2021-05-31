49ba841f37275d83459ec719795e36caebce1ce6
MRmlik12 <mrmlik12sup@gmail.com>
2021-05-31 22:23:09 +0200

    Add jwt authentication

--- search/src/Vulder.Search.Api/Startup.cs
+++ search/src/Vulder.Search.Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -57,6 +58,8 @@ namespace Vulder.Search.Api
                     }
                 };
             });
+            
+            services.AddSingleton<IUserIdProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
